9a00359ad7c7afde322448cedb8f0c0ae7004b74
fszwed <s10315fs@ms.wwsi.edu.pl>
2025-11-16 13:46:48 +0100

    Add MapImage component to display a map image in the app

---
+++ components/MapImage.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+const MapImage = () => {
+    return (
+        <View style={styles.container}>
+            <Image
+                style={styles.map}
+                source={{
+                    uri: 'https://i.erli.pl/kd7jsy.25d2b8.l.webp',
+                }}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    map: {
+        width: 300,
+        height: 200,
+    },
+});
+
+export default MapImage;
